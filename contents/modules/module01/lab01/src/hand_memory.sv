8d99f2547035bab0275d8feead506cf3f8d5cbfb
ChipMango <support@chipmango.com>
2025-06-06 17:44:03 +0100

    Updated hand_memory.sv

--- contents/modules/module01/lab01/src/hand_memory.sv
+++ contents/modules/module01/lab01/src/hand_memory.sv
@@ -11,3 +11,13 @@
 //   v1.0 - 06/06/25: Initial file created with module template
 //------------------------------------------------------------------------------
 
+
+module hand_memory (
+    input  logic        clk,
+    input  logic        rst_n,
+    input  logic        we,             // Write Enable
+    input  logic [2:0]  waddr,          // 3-bit address: valid range 0–4
+    input  logic [5:0]  card_in,        // Encoded card input
+    output logic        hand_full,      // High when all 5 slots are filled
+    output logic [5:0]  hand [4:0]      // The 5-card hand (array of outputs)
+);
