3cbefc13ca81a235c6633ccdf3d3043378883a99
ChipMango <support@chipmango.com>
2025-06-07 15:47:59 +0100

    Add Lab 04 files: fsm_controller.sv, player_fsm_controller.sv, test_game_fsm.sv testbench

---
+++ contents/modules/module04/lab04/src/player_fsm_controller.sv
@@ -0,0 +1,13 @@
+
+//------------------------------------------------------------------------------
+// File Name:player_fsm_controller.sv
+// Description:player interface module that integrates memory, evaluator, and command generator with the FSM.
+// Author: <Student Name>
+// Date: <Date>
+// Version: 1.0
+// Project: ChipMango Lab 04 - Digitally Representing Poker Cards
+// License: ChipMango Confidential - For educational purposes only
+//------------------------------------------------------------------------------
+// Revision History:
+//   v1.0 - 07/06/25: Initial file created with module template
+//------------------------------------------------------------------------------
---
+++ contents/modules/module04/lab04/testbench/test_game_fsm.sv
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// File Name: test_game_fsm.sv
+// Description: Testbench for simulating the FSM and verifying the state transitions and game flow.
+// Author: <Student Name>
+// Date: <Date>
+// Version: 1.0
+// Project: ChipMango Lab 04 - Digitally Representing Poker Cards
+// License: ChipMango Confidential - For educational purposes only
+//------------------------------------------------------------------------------
+// Revision History:
+//   v1.0 - 07/06/25: Initial file created with module template
+//------------------------------------------------------------------------------
+
